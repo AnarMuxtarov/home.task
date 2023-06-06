4e4c24fd64a15039b51a0a6308603fbe8cedfc45
AnarMuxtarov <anar1997cv@gmail.com>
2023-06-07 00:02:20 +0400

    add new javascript file ((:

---
+++ task11/main.js
@@ -0,0 +1,27 @@
+
+  
+
+
+
+
+
+// tapsiriq 1 function declaretion
+function Printfullname(name,surname){
+    return name+" "+surname
+}
+console.log(Printfullname("Anar","Muxtarov"))
+
+
+
+
+
+
+
+
+//   5 ci tapsiriq  baslangic
+let getPow= function ( x , n){
+    let value=x**n;
+    console.log(value)
+    return value
+}
+getPow(3,5);
