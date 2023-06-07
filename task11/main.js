dac57fe5121e404445353bbc0fe3a1797aa414e8
AnarMuxtarov <anar1997cv@gmail.com>
2023-06-07 23:46:11 +0400

    add javascript file lesson  1,2,3,5

--- task11/main.js
+++ task11/main.js
@@ -13,8 +13,29 @@ console.log(Printfullname("Anar","Muxtarov"))
 
 
 
+// tapsiriq 2 arrov function  ternerary operator
+ let printFullNameWithParam=(f_name,l_name)=>{
+    let fullname=`${f_name}  ${l_name}  m`
+  console.log(fullname);
+  return fullname;
+ };
+ console.log(printFullNameWithParam=("Anar","Muxtarov"))
 
 
+
+
+
+// tapsiriq 3 perimetrin hesablanmasi  2*(length+width) function declaration
+perimeterofaRectangle= function(length, width) { 
+    return (2*(length+width));
+}
+console.log(perimeterofaRectangle(6,4))
+
+
+
+
+// 4 ci tapsiriq  baslangic
+// basa dusemmedim
 
 
 
