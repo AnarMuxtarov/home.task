72418502ae2052c81e1387ebce69a84b10a26df9
AnarMuxtarov <anar1997cv@gmail.com>
2023-06-08 23:58:23 +0400

    add new array task

---
+++ task12 javascript/main.js
@@ -0,0 +1,40 @@
+
+let employees=[
+
+    {
+        name:'Anar',
+        age:'26',
+        job:'tiens-company',
+        salary:'seventy fund',
+
+    },
+
+    {
+        name:'Elgun',
+        age:'24',
+        job:'pasha-holding',
+        salary:'ninety fund',
+    },
+
+
+    {
+        name:'orxan',
+        age:'28',
+        job:'defendo',
+        salary:'eighty fund',
+    }
+]
+
+function (arr){  
+
+ for(i=0; i<arr.length;i++)
+
+   metods=`${data[i].name} ${data[i].age} ${data[i].job} ${data[i].salary}`
+    
+ console.log(metods); 
+
+}
+// ?????????(((
+
+
+
